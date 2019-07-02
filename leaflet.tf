132cebe8b6fade1e5a7adb208686f67675ead705
torresal <alexander.p.torres@jol.nasa.gov>
2019-07-02 16:10:03 -0700

    mozart provisioing test 2

--- leaflet.tf
+++ leaflet.tf
@@ -43,29 +43,6 @@ resource "aws_instance" "leaflet" {
     command = "echo ${aws_instance.leaflet.private_ip} > leaflet_ip_address.txt"
   }
 
-  provisioner "local-exec" {
-    command = [
-      # Add config file to .sds
-      "cd /home/ops/.sds",
-      "(echo 'VERDI_PVT_IP: ${aws_instance.leaflet.private_ip}'; echo 'TS_PVT_IP: ${aws_instance.leaflet.private_ip}') > tss_config",
-      # provision displacement-ts-server
-      "cd /home/ops/mozart/ops",
-      "git clone https://github.com/hysds/displacement-ts-server.git -b dev",
-      "cd /home/ops/mozart/ops/displacement-ts-server",
-      "sudo pip install -r requirements.txt",
-      "cd configs/certs",
-      "openssl genrsa -des3 -passout pass:${var.pass_phrase} -out server.key 1024",
-      "OPENSSL_CONF=server.cnf openssl req -passin pass:${var.pass_phrase} -new -key server.key -out server.csr",
-      "cp server.key server.key.org",
-      "openssl rsa -passin pass:${var.pass_phrase} -in server.key.org -out server.key",
-      "chmod 600 server.key*",
-      "openssl x509 -req -days 99999 -in server.csr -signkey server.key -out server.crt",
-      "cd /home/ops/mozart/ops/displacement-ts-server/update_tss",
-      "bash update_tss.sh"
-    ]
-  }
-
-
   provisioner "remote-exec" {
     inline = [
       # provision verdi
@@ -79,6 +56,7 @@ resource "aws_instance" "leaflet" {
       "sudo ln -sf ${var.leaflet["data"]}/var/lib/redis /var/lib/redis",
       "sudo systemctl start redis",
       "sudo bash -c \"echo ${lookup(var.leaflet, "data_dev_mount", var.leaflet["data_dev"])} ${var.leaflet["data"]} auto defaults,nofail,comment=terraform 0 2 >> /etc/fstab\"",
+      "sudo pip install docker-compose"
       # provision displacement-ts-server
 #      "cd /home/ops/verdi/ops",
 #      "git clone https://github.com/hysds/displacement-ts-server.git -b dev",
@@ -97,6 +75,28 @@ resource "aws_instance" "leaflet" {
 #      "sudo /usr/sbin/apachectl stop",
 #      "supervisord -c /home/ops/verdi/etc/supervisord.conf"
 #      "docker-compose up -d"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = [
+      # Add config file to .sds
+      "cd /home/ops/.sds",
+      "(echo 'VERDI_PVT_IP: ${aws_instance.leaflet.private_ip}'; echo 'TS_PVT_IP: ${aws_instance.leaflet.private_ip}') > tss_config",
+      # provision displacement-ts-server
+      "cd /home/ops/mozart/ops",
+      "git clone https://github.com/hysds/displacement-ts-server.git -b dev",
+#      "cd /home/ops/mozart/ops/displacement-ts-server",
+#      "sudo pip install -r requirements.txt",
+      "cd configs/certs",
+      "openssl genrsa -des3 -passout pass:${var.pass_phrase} -out server.key 1024",
+      "OPENSSL_CONF=server.cnf openssl req -passin pass:${var.pass_phrase} -new -key server.key -out server.csr",
+      "cp server.key server.key.org",
+      "openssl rsa -passin pass:${var.pass_phrase} -in server.key.org -out server.key",
+      "chmod 600 server.key*",
+      "openssl x509 -req -days 99999 -in server.csr -signkey server.key -out server.crt",
+      "cd /home/ops/mozart/ops/displacement-ts-server/update_tss",
+      "bash update_tss.sh"
     ]
   }
 }
